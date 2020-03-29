d4e8a9ef21744e4a431fd45e1a12d1b6d578f556
unknown <gauravn67@gmail.com>
2020-03-29 13:45:15 +0530

    Question 16 added

---
+++ Q16.R
@@ -0,0 +1,13 @@
+library(MASS)
+library(caTools)
+
+set.seed(5)
+split=sample.split(Boston$medv,SplitRatio = 0.5)
+training_set = subset(Boston[-7], split == TRUE)
+testing_set = subset(Boston[-7], split == FALSE)
+
+myModel=lm(training_set$medv~.,data=training_set)
+
+predictions=predict(myModel,testing_set)
+
+mean((testing_set$medv-predictions)**2)
